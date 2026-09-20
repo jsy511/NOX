61c29614ff1687b7ca58244df98d3f10105cad98
jsy511 <jaydenfreeman9876@gmail.com>
2026-09-20 08:55:13 +0100

    Create nox/cli/main.py

---
+++ nox/nox/cli/main.py
@@ -0,0 +1,11 @@
+from nox.core.banner import BANNER
+
+
+def main():
+    print(BANNER)
+    print("NOX is ready.")
+    print("Type 'help' to see available commands.")
+
+
+if __name__ == "__main__":
+    main()
