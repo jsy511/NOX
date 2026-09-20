ac98b041dc977924e0234fec61c17abc08c120d6
jsy511 <jaydenfreeman9876@gmail.com>
2026-09-20 08:52:43 +0100

    Create nox/__init__.py

---
+++ nox/__init__.py
@@ -0,0 +1,9 @@
+"""
+NOX - Mobile Cybersecurity Toolkit
+
+Core package for authorised cybersecurity education,
+defensive security, CTFs, and security testing.
+"""
+
+__version__ = "0.1.0"
+__author__ = "NOX Project"
